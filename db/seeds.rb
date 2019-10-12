96e021b7969b2b513ca34286c04807aa202ae0e2
na229aih <naoya.aihara.1127@gmail.com>
2019-10-12 14:00:48 +0900

    [追加]seed
    レコード作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+# Product.create(
+#   name: "うまい棒",
+#   discription: "やかんです",
+#   condition: "未使用に近い",
+#   delivery_charge: "出品者",
+#   origin_area: "宮城県",
+#   days_left_send: "1~2日",
+#   price: "350",
+#   status: "出品中",
+# )
+
+# Detail.create(
+#   large_category: "メンズ",
+#   medium_category: "トップス",
+#   small_category: "シャツ",
+#   size: "S",
+#   brand: "シャネル",
+#   product_id: "3",
+# )
+
+# Image.create(
+#   img: "https://img1.esimg.jp/resize/320x240/image/food/01/34/03/1843693.jpg?ts=1545804614276",
+#   product_id: "4",
+# )
