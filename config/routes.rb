bafcf0db27a73ea17ea7df48121086123dbd66b1
na229aih <naoya.aihara.1127@gmail.com>
2019-10-14 15:50:12 +0900

    [追加]ルーティング signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'products#index'
 
-  resources :products ,only: [:new,]
+  resources :products ,only: [:new]
+
+  resources :signup, only: [:create] do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'step5'
+      get 'step6'
+      get 'done'
+    end
+  end
 end
