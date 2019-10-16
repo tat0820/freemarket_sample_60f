76b34a50a214d1029ff5b2f5bf4933017a964281
鈴木達也 <tat@suzukitatsuyanoMacBook-Air.local>
2019-10-16 17:57:28 +0900

    修正

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,28 +1,25 @@
-
 $(function(){
 $(".chosen-dot").mouseover(function(){
-    var ImgSrc = $(this).attr("src");
-    var ImgAlt = $(this).attr("alt");
-    $("img#MainPhoto").attr({src:ImgSrc,alt:ImgAlt});
+    var chosen_dot_child = $(this).children("div")
+    var chosen_dot_childchild = chosen_dot_child.children("img")
+    var ImgSrc = chosen_dot_childchild.attr("src");
+    $("img#MainPhoto").attr({src:ImgSrc});
     $("img#MainPhoto").hide();
-    $("img#MainPhoto").fadeIn("slow");
+    $("img#MainPhoto").fadeIn();
     return false;
 });
 });
 
-
-{/* <div class="wrap">
-<p><img src="img/01.jpg" width="400" height="400" alt="下筌ダム" id="MainPhoto"></p>
-<ul>
-<li><img src="img/01.jpg" width="72" alt="下筌ダム" class="ChangePhoto"></li>
-<li><img src="img/02.jpg" width="72" alt="石打ダム" class="ChangePhoto"></li>
-<li><img src="img/03.jpg" width="72" alt="船津ダム" class="ChangePhoto"></li>
-<li><img src="img/04.jpg" width="72" alt="緑川ダム" class="ChangePhoto"></li>
-<li><img src="img/05.jpg" width="72" alt="荒瀬ダム" class="ChangePhoto"></li>
-</ul>
-</div> */}
-
-
+// {/* <div class="wrap">
+// <p><img src="img/01.jpg" width="400" height="400" alt="下筌ダム" id="MainPhoto"></p>
+// <ul>
+// <li><img src="img/01.jpg" width="72" alt="下筌ダム" class="ChangePhoto"></li>
+// <li><img src="img/02.jpg" width="72" alt="石打ダム" class="ChangePhoto"></li>
+// <li><img src="img/03.jpg" width="72" alt="船津ダム" class="ChangePhoto"></li>
+// <li><img src="img/04.jpg" width="72" alt="緑川ダム" class="ChangePhoto"></li>
+// <li><img src="img/05.jpg" width="72" alt="荒瀬ダム" class="ChangePhoto"></li>
+// </ul>
+// </div> */}
 
 // $(function() {
 //   $('.slide-box').on("mouseover",function(){
