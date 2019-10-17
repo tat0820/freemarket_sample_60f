7c85ee3295c6935c77830f79d37b5c9e1e404ba0
鈴木達也 <tat@suzukitatsuyanoMacBook-Air.local>
2019-10-17 17:27:53 +0900

    修正

--- db/migrate/20191016053446_add_column_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :nickname, :string
-    add_column :users, :password, :string
-    add_column :users, :phone_number, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_10_16_053446) do) do
+ActiveRecord::Schema.define(version: 2019_10_14_112613) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
@@ -24,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_10_16_053446) do) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
-
 
   create_table "details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "large_category", null: false
@@ -79,7 +77,6 @@ ActiveRecord::Schema.define(version: 2019_10_16_053446) do) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -102,9 +99,6 @@ ActiveRecord::Schema.define(version: 2019_10_16_053446) do) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
-    t.string "password"
-    t.integer "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
