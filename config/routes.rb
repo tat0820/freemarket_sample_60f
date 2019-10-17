45fbf78f0898cb42f173ed53629435da410c4205
ele9tro <naoyuki2633.nagatani@gmail.com>
2019-10-17 11:41:43 +0900

    [編集]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  controllers: {
+  sessions: 'users/sessions',
+  registrations: 'users/registrations' ,
+  omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   root 'products#index'
 
   resources :products ,only: [:new,]
