c9d279f8f932dd4f6a27aef61fb6358f2792f0a3
takahiro-ny <tube412good@gmail.com>
2019-10-20 14:51:13 +0900

    [追加]出品の料金計算

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -3,19 +3,47 @@ $(function(){
         var chosen_dot_child = $(this).children("div")
         var chosen_dot_childchild = chosen_dot_child.children("img")
         var ImgSrc = chosen_dot_childchild.attr("src");
+        console.log(chosen_dot_child)
         $("img#MainPhoto").attr({src:ImgSrc});
         $("img#MainPhoto").hide();
         $("img#MainPhoto").fadeIn();
         return false;
     });
 
+    function money2(fee){
+        var html = `<span class="fee">
+                        ¥${fee}
+                    </span>`
+        $(".sale-page-box__price__right__fee").append(html);
+    }
+
+    function money3(profit){
+        var html = `<span class="profit">
+                        ¥${profit}
+                    </span>`
+        $(".sale-page-box__price__right__profit").append(html);
+    }
+
     $(".sale-page-box__price__right__value").keyup(function(){
-        var writeText = "";
-        
-        
-        
-        
-        
-  )};
+        var input_number = $(this).val();
+        $(".minus").remove();
+        if(input_number >= 300 && input_number <= 9999999)
+        {
+            var fee = Math.floor(input_number * 0.1);
+            var profit = Math.floor(input_number * 0.9);
+            $(".fee").remove();
+            money2(fee);
+            $(".profit").remove();
+            money3(profit);
+        }
+        else
+        {
+            var aaa = `<div class="minus">ー</div>`;
+            $(".fee").remove();
+            $(".profit").remove();
+            $(".sale-page-box__price__right__fee").append(aaa);
+            $(".sale-page-box__price__right__profit").append(aaa);
+        }
+    });
 });
 
