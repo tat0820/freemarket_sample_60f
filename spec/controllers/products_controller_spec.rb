318679ce0de5f85504bcc1eb55dd660ca16e287b
ele9tro <naoyuki2633.nagatani@gmail.com>
2019-10-21 20:36:33 +0900

    [編集]単体テスト

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 describe ProductsController do
+  let(:user) { create(:user) }
 
-  # describe 'GET #index' do
+  describe 'GET #index' do
 
-  #   it "indexビューが呼ばれているか" do
-  #     get :index
-  #     expect(response).to render_template :index
-  #   end
+    it "indexビューが呼ばれているか" do
+      get :index
+      expect(response).to render_template :index
+    end
 
-  #   it "@productsに正しい値が入っているか" do
-  #     products = create_list(:product, 3)
-  #     get :index
-  #     expect(assigns(:products)).to match(products.sort{ |a, b| b.created_at <=> a.created_at })
-  #   end
+    it "@productsに正しい値が入っているか" do
+      products = create_list(:product, 3)
+      get :index
+      expect(assigns(:products)).to match(products.sort{ |a, b| b.created_at <=> a.created_at })
+    end
 
-  # end
+  end
 
   describe 'GET #new' do
     it "renders the :new template" do
@@ -39,4 +40,16 @@ describe ProductsController do
     end
   end
 
+  describe 'DELETE #destroy' do
+    it "productテーブルのレコードが削除できたか" do
+      product = create(:product)
+      # binding.pry
+      expect{
+        delete :destroy, id: product.id
+      }.to change(Product,:count).by(-1)
+      # post :destroy params: {id: product}
+      # expect{desroy :product}.to change(Product, :count).by(-1)
+      # expect{delete :destroy, product}.to change(Product, :count).by(-1)
+    end
+  end
 end
